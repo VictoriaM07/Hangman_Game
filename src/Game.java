e6386af8d362f928c64f2d88246e490681356d4b
VictoriaM07 <morari.victoria07@gmail.com>
2025-01-16 19:48:23 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    public static void main(String[] args) {
+
+        // 1. Print Welcome to Hangman
+        // 2. Salveaza un cuvand random intr-o variab.
+        // 3. Afiseaza nr stelute = numar caractere
+        // 4. Cere litera de la user
+        // 5. Daca litera se regasete se inlocuieste fiecare steluta cu acea litera de la acelasi index
+
+
+
+    }
+}
